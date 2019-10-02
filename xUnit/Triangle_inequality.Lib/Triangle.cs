c0ca3f44d925adb170b65e717d017a3440319233
polinskaya <polinskaya27@mail.ru>
2019-10-02 22:52:08 +0300

    add unit tests with NUnit

---
+++ xUnit/Triangle_inequality.Lib/Triangle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Triangle_inequality.Lib
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(double a, double b, double c) =>
+            (a > 0 && b > 0 && c > 0 && (a + b > c) && (a + c > b) && (b + c > a));
+    }
+}
---
+++ xUnit/Triangle_inequality.Tests/TriangleTest.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using Triangle_inequality.Lib;
+
+namespace Triangle_inequality.Tests
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        [Test]
+        public void EquilateralTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(2.5, 2.5, 2.5));
+        }
+
+
+        [Test]
+        public void IsoscelesTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(5, 7, 5));
+        }
+
+
+        [Test]
+        public void OneNegativeLengthSide()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(-7, 27, 3.3));
+        }
+
+
+        [Test]
+        public void SumEqualsSideLength()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(3, 4, 7));
+        }
+
+
+        [Test]
+        public void SumLessThanSideLength()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(5, 20, 15));
+        }
+
+        [Test]
+        public void SumMoreThanSideLength()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(10, 20, 25));
+        }
+
+
+        [Test]
+        public void ThreeNegativeLengthSides()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(-1, -2, -8));
+        }
+
+
+        [Test]
+        public void ThreeZeroLengthSides()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(0, 0, 0));
+        }
+
+
+        [Test]
+        public void TwoNegativeLengthSides()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(-17, 14, -16));
+        }
+
+
+        [Test]
+        public void ZeroLengthSide()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(3, 0, 2));
+        }
+    }
+}
