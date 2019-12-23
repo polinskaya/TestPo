506575d7a228f6eb9492e461a9a5d9295a242619
polinskaya <polinskaya27@mail.ru>
2019-12-24 00:14:09 +0300

    CI test

--- Framework2/SeleniumWebdriver/Services/TestDataReader.cs
+++ Framework2/SeleniumWebdriver/Services/TestDataReader.cs
@@ -13,11 +13,17 @@ namespace SeleniumWebdriver.Services
                 var variable = TestContext.Parameters.Get("environment");
                 var file = string.IsNullOrEmpty(variable) ? "dev" : variable;
                 var separateIndex = AppDomain.CurrentDomain.BaseDirectory.IndexOf("bin", StringComparison.Ordinal);
-                var customConfigFileMap = new ExeConfigurationFileMap
+                var customConfigFileMap = new ExeConfigurationFileMap();
+                if (separateIndex == -1)
                 {
-                    ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory.Substring(0, separateIndex) +
-                                        @"Configuration\" + file + ".config"
-                };
+                    customConfigFileMap.ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory + @"Configuration\" + file + ".config";
+                }
+                else
+                {
+                    customConfigFileMap.ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory.Substring(0, separateIndex) +
+                                        @"Configuration\" + file + ".config";
+                }
+
                 return ConfigurationManager.OpenMappedExeConfiguration(customConfigFileMap, ConfigurationUserLevel.None);
             }
         }
