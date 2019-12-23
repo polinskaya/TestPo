9b94c5f0a07190452aee84a331ae3b9c93e35ef6
polinskaya <polinskaya27@mail.ru>
2019-12-23 22:54:00 +0300

    CI

--- Framework2/Test.cmd
+++ Framework2/Test.cmd
@@ -5,6 +5,6 @@ popd
 cd SeleniumWebdriver
 "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe" SeleniumWebdriver.csproj /p:OutputPath=d:\GOCD_ARTIFACTS\package /t:rebuild
 cd ../packages\NUnit.ConsoleRunner.3.10.0\tools
-nunit3-console.exe --testparam:browser=Chrome --testparam:environment=dev "../../../SeleniumWebdriver/bin/Debug/SeleniumWebdriver.dll"
+nunit3-console.exe --testparam:browser=Chrome --testparam:environment=dev "d:\GOCD_ARTIFACTS\package/SeleniumWebdriver.dll"
 popd
 
